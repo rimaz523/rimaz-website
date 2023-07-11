3bfdb1c175fdf2c71d6d889a5c920b3760c754a7
Mohommed Rimaz <rimazmohommed523@gmail.com>
2023-07-11 11:23:02 +0000

    setup material ui (#10)

--- website-react-spa/src/App.js
+++ website-react-spa/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <Button variant="contained">Hello World</Button>
+        </div>
       </header>
     </div>
   );
