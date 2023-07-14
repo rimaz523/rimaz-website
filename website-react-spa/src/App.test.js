7a794f25a6135a6a2c67229703ef3069fcaae1f6
Mohommed Rimaz <rimazmohommed523@gmail.com>
2023-07-14 21:00:01 +1200

    fix linting errors

--- website-react-spa/src/App.test.js
+++ website-react-spa/src/App.test.js
@@ -1,5 +1,8 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
+
+/* eslint-env jest */
 
 test('renders learn react link', () => {
   render(<App />)
