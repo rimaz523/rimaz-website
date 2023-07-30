9b1e80798eecc9dc46500b2d6eabd0488329b1d0
Mohommed Rimaz <rimazmohommed523@gmail.com>
2023-07-30 06:44:50 +0000

    use redux-persist to save theme in localstorage (#27)

--- website-react-spa/src/app/store.js
+++ website-react-spa/src/app/store.js
@@ -1,8 +1,24 @@
 import { configureStore } from '@reduxjs/toolkit'
 import themeReducer from '../features/theme/themeSlice'
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from 'redux-persist'
+import thunk from 'redux-thunk'
+import { combineReducers } from 'redux'
 
-export default configureStore({
-  reducer: {
-    theme: themeReducer,
-  },
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['theme'],
+}
+const rootReducer = combineReducers({
+  theme: themeReducer,
 })
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: [thunk],
+})
+
+export const persistor = persistStore(store)
--- website-react-spa/src/index.js
+++ website-react-spa/src/index.js
@@ -3,16 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter as Router } from 'react-router-dom'
-import store from './app/store'
+import { persistor, store } from './app/store'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <Router>
-        <App />
-      </Router>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router>
+          <App />
+        </Router>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
 )
