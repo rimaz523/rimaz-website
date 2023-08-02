95849bb529f0b886e45f37952212da9f2ad6eddb
Mohommed Rimaz <rimazmohommed523@gmail.com>
2023-08-02 03:55:57 +0000

    add health endpoint for api (#34)

---
+++ integrations-dotnet-api/src/WebApi/Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Polling health check endpoint");
+            return new OkResult();
+        }
+    }
+}
