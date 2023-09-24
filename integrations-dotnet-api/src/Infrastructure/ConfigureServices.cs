846daac9395644d88ff223e35430dd83a7ee8512
Mohommed Rimaz <rimazmohommed523@gmail.com>
2023-09-24 19:59:47 +1300

    configure polly for http retries

--- integrations-dotnet-api/src/Infrastructure/ConfigureServices.cs
+++ integrations-dotnet-api/src/Infrastructure/ConfigureServices.cs
@@ -2,6 +2,8 @@
 using Infrastructure.ApiServices;
 using Infrastructure.Common;
 using Microsoft.Extensions.DependencyInjection;
+using Polly.Extensions.Http;
+using Polly;
 
 namespace Infrastructure
 {
@@ -9,9 +11,19 @@ namespace Infrastructure
     {
         public static IServiceCollection AddInfrastructure(this IServiceCollection services)
         {
-            services.AddHttpClient<ILogicAppApiService, LogicAppApiService>();
+            services.AddHttpClient<ILogicAppApiService, LogicAppApiService>()
+                .SetHandlerLifetime(TimeSpan.FromMinutes(1))
+                .AddPolicyHandler(GetRetryPolicy());
             services.ConfigureOptions<IntegrationOptionsSetup>();
             return services;
+        }
+
+        static IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            return HttpPolicyExtensions
+                .HandleTransientHttpError()
+                .OrResult(msg => msg.StatusCode == System.Net.HttpStatusCode.NotFound)
+                .WaitAndRetryAsync(3, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
         }
     }
 }
