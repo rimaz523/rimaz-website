38a37bbc8c51f96a3a84e77e65e17419c8098109
Mohommed Rimaz <rimazmohommed523@gmail.com>
2023-10-17 09:02:40 +0000

    setup and connect to sql server (#85)
    * set up sql server in integrations project
    
    * format code
    
    * get blog posts from sql server db
    
    * remove default value for limit

---
+++ integrations-dotnet-api/src/Domain/Common/BaseEntity.cs
@@ -0,0 +1,5 @@
+﻿namespace Domain.Common;
+public abstract class BaseEntity
+{
+    public Guid Id { get; set; }
+}
--- integrations-dotnet-api/src/Domain/Entities/BlogPost.cs
+++ integrations-dotnet-api/src/Domain/Entities/BlogPost.cs
@@ -1,8 +1,9 @@
-﻿namespace Domain.Entities;
+﻿using Domain.Common;
 
-public class BlogPost
+namespace Domain.Entities;
+
+public class BlogPost : BaseEntity
 {
-    public Guid Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
     public string Image { get; set; } = string.Empty;
