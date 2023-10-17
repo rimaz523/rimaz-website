04b3c9c9c9f3bb98e5730eaa2e1356c33ff177a1
Mohommed Rimaz <rimazmohommed523@gmail.com>
2023-10-17 21:08:32 +1300

    set up sql server in integrations project

--- integrations-dotnet-api/src/Domain/Entities/Message.cs
+++ integrations-dotnet-api/src/Domain/Entities/Message.cs
@@ -1,8 +1,9 @@
-﻿namespace Domain.Entities;
+﻿using Domain.Common;
 
-public class Message
+namespace Domain.Entities;
+
+public class Message : BaseEntity
 {
-    public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Email { get; set; } = string.Empty;
     public string Subject { get; set; } = string.Empty;
--- integrations-dotnet-api/src/Infrastructure/ConfigureServices.cs
+++ integrations-dotnet-api/src/Infrastructure/ConfigureServices.cs
@@ -4,17 +4,28 @@ using Infrastructure.Common;
 using Microsoft.Extensions.DependencyInjection;
 using Polly.Extensions.Http;
 using Polly;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Application.Common.Interfaces.Persistence;
 
 namespace Infrastructure
 {
     public static class ConfigureServices
     {
-        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddHttpClient<ILogicAppApiService, LogicAppApiService>()
                 .SetHandlerLifetime(TimeSpan.FromMinutes(1))
                 .AddPolicyHandler(GetRetryPolicy());
+
             services.ConfigureOptions<IntegrationOptionsSetup>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetSection("Persistence").GetSection("SqlServerConnectionString").Value,
+                    builder => builder.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+            services.AddScoped<IApplicationDbContext>(provider => provider.GetRequiredService<ApplicationDbContext>());
+            services.AddScoped<ApplicationDbContextInitialiser>();
+
             return services;
         }
 
