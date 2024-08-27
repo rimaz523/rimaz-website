ff6e02810452ae5f6575d3d6c1c58bb595973797
Mohommed Rimaz <rimazmohommed523@gmail.com>
2024-08-27 21:25:49 +1200

    add slug for blog article (#134)

--- integrations-dotnet-api/src/Domain/Entities/BlogPost.cs
+++ integrations-dotnet-api/src/Domain/Entities/BlogPost.cs
@@ -6,6 +6,7 @@ public class BlogPost
     public string Title { get; set; } = string.Empty;
     public string Content { get; set; } = string.Empty;
     public string Image { get; set; } = string.Empty;
+    public string Slug { get; set; } = string.Empty;
     public string Url { get; set; } = string.Empty;
     public DateTimeOffset PublishedDate { get; set; }
 }
