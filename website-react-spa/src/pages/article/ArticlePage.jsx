001c1f6ab28177468b6a17c3873d89658261f387
Mohommed Rimaz <rimazmohommed523@gmail.com>
2024-09-15 10:20:20 +1200

    handle invalid slug queries (#141)

--- website-react-spa/src/pages/article/ArticlePage.jsx
+++ website-react-spa/src/pages/article/ArticlePage.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 import { Box, Typography, Card, CardContent } from '@mui/material'
 import LinearProgress from '@mui/material/LinearProgress'
@@ -12,8 +12,13 @@ import { useGetArticleBySlugQuery } from '../../features/integrations/integratio
 
 const ArticlePage = () => {
   const { slug } = useParams()
-  const { data = {}, isFetching } = useGetArticleBySlugQuery({ slug })
+  const { data = {}, isFetching, isError, isSuccess } = useGetArticleBySlugQuery({ slug })
   const isDarkMode = useSelector((state) => state.theme.darkmode)
+  const navigate = useNavigate()
+
+  if (isError) {
+    navigate('/page-not-found')
+  }
 
   return isFetching ? (
     <Box sx={{ width: '100%' }}>
@@ -21,62 +26,66 @@ const ArticlePage = () => {
     </Box>
   ) : (
     <Box key={slug} textAlign='left' pt={{ xs: 15 }}>
-      <Typography textAlign='center' variant='h6' sx={{ fontWeight: 'bold' }}>
-        {data.title}
-      </Typography>
-      <Card
-        variant='outlined'
-        sx={{ mx: { xs: 2, md: 10 }, my: 4, borderRadius: '10px', height: '100%' }}
-      >
-        <CardContent>
-          {data.sections.map(function (section) {
-            return (
-              <Stack
-                direction='column'
-                key={section.id}
-                spacing={section.type === 'code' ? 0 : 1}
-                mt={2}
-                px={2}
-                width={section.type === 'image' ? { xs: '100%', md: '60%' } : {}}
-              >
-                {section.type === 'segment' &&
-                  section.contents.map(function (segment) {
-                    return <Typography key={segment}>{segment}</Typography>
-                  })}
-                {section.type === 'code' &&
-                  section.contents.map(function (code) {
-                    return (
-                      <Typography
-                        pl={2}
-                        key={code}
-                        backgroundColor={isDarkMode ? '#3a3b3c' : '#d3d3d3'}
-                      >
-                        {code}
-                      </Typography>
-                    )
-                  })}
-                {section.type === 'image' && (
-                  <Image
-                    src={process.env.REACT_APP_BLOB_STORE_BASE_URL.concat(
-                      '/',
-                      data.slug,
-                      '/',
-                      section.contents,
+      {isSuccess && (
+        <>
+          <Typography textAlign='center' variant='h6' sx={{ fontWeight: 'bold' }}>
+            {data.title}
+          </Typography>
+          <Card
+            variant='outlined'
+            sx={{ mx: { xs: 2, md: 10 }, my: 4, borderRadius: '10px', height: '100%' }}
+          >
+            <CardContent>
+              {data.sections.map(function (section) {
+                return (
+                  <Stack
+                    direction='column'
+                    key={section.id}
+                    spacing={section.type === 'code' ? 0 : 1}
+                    mt={2}
+                    px={2}
+                    width={section.type === 'image' ? { xs: '100%', md: '60%' } : {}}
+                  >
+                    {section.type === 'segment' &&
+                      section.contents.map(function (segment) {
+                        return <Typography key={segment}>{segment}</Typography>
+                      })}
+                    {section.type === 'code' &&
+                      section.contents.map(function (code) {
+                        return (
+                          <Typography
+                            pl={2}
+                            key={code}
+                            backgroundColor={isDarkMode ? '#3a3b3c' : '#d3d3d3'}
+                          >
+                            {code}
+                          </Typography>
+                        )
+                      })}
+                    {section.type === 'image' && (
+                      <Image
+                        src={process.env.REACT_APP_BLOB_STORE_BASE_URL.concat(
+                          '/',
+                          data.slug,
+                          '/',
+                          section.contents,
+                        )}
+                        showLoading
+                      />
+                      // for mobile image, do 100%
                     )}
-                    showLoading
-                  />
-                  // for mobile image, do 100%
-                )}
-                {section.type === 'heading' && (
-                  <Typography variant='h6' pt={2} sx={{ fontWeight: 'bold' }}>
-                    {section.contents}
-                  </Typography>
-                )}
-              </Stack>
-            )
-          })}
-        </CardContent>
-      </Card>
+                    {section.type === 'heading' && (
+                      <Typography variant='h6' pt={2} sx={{ fontWeight: 'bold' }}>
+                        {section.contents}
+                      </Typography>
+                    )}
+                  </Stack>
+                )
+              })}
+            </CardContent>
+          </Card>
+        </>
+      )}
     </Box>
   )
 }
--- website-react-spa/src/pages/page-not-found/PageNotFound.jsx
+++ website-react-spa/src/pages/page-not-found/PageNotFound.jsx
@@ -1,13 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import PropTypes from 'prop-types'
 
-import { Box } from '@mui/material'
+import { Box, Button } from '@mui/material'
 
 const PageNotFound = ({ content }) => {
+  const navigate = useNavigate()
+
   return (
     <Box textAlign='center' pt={{ xs: 25 }} pb={{ xs: 25 }}>
       <h1>{content}</h1>
+      <Button variant='contained' onClick={() => navigate('/')}>
+        Back to home
+      </Button>
     </Box>
   )
 }
