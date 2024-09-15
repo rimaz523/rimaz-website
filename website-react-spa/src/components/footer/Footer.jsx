e0dade28f5e4d1ce4a4a35033d6196c6d8e67237
Mohommed Rimaz <rimazmohommed523@gmail.com>
2024-09-15 21:34:48 +1200

    add ssl logo to footer (#143)

--- website-react-spa/src/components/footer/Footer.jsx
+++ website-react-spa/src/components/footer/Footer.jsx
@@ -2,19 +2,35 @@ import React from 'react'
 
 import PropTypes from 'prop-types'
 
-import { Box, Container } from '@mui/material'
+import { Box, Typography } from '@mui/material'
+import Stack from '@mui/material/Stack'
+
+import Image from 'mui-image'
 
 const Footer = ({ preContent, postContent }) => {
   return (
     <footer>
-      <Box borderTop={1}>
-        <Container>
-          <Box textAlign='center' pt={{ xs: 2, sm: 5 }} pb={{ xs: 2, sm: 5 }}>
+      <Box pt={{ xs: 2, sm: 5 }} pb={1} borderTop={1}>
+        <Stack direction='column' spacing={2} alignItems='center' justifyContent='center'>
+          <Typography>
             {preContent}
             {new Date().getFullYear()}
             {postContent}
-          </Box>
-        </Container>
+          </Typography>
+          <Image
+            src={process.env.REACT_APP_BLOB_STORE_BASE_URL.concat(
+              '/app/positivessl_trust_seal.png',
+            )}
+            width='124px'
+            height='32px'
+            onClick={() => {
+              window.open(
+                'https://secure.trust-provider.com/ttb_searcher/trustlogo?v_querytype=W&v_shortname=POSDV&v_search=https://rimaz.dev/&x=6&y=5',
+              )
+            }}
+            style={{ cursor: 'pointer' }}
+          />
+        </Stack>
       </Box>
     </footer>
   )
