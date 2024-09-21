7bf70052228c42bc86965a3fc5661fa547e47dc8
Mohommed Rimaz <rimazmohommed523@gmail.com>
2024-09-21 21:19:57 +1200

    update whitelist script to also whitelist local machine ip for cosmos (#145)

--- terraform-infra/scripts/whitelist_ip_in_sql_server.ps1
+++
@@ -1,3 +0,0 @@
-az login
-$ipAddress = (Invoke-WebRequest -uri "https://ifconfig.me/ip").Content
-az sql server firewall-rule create -g rimaz-data-dev-rg -s rimaz-dev-app-db-server -n whitelist-local-machine --start-ip-address $ipAddress --end-ip-address $ipAddress
