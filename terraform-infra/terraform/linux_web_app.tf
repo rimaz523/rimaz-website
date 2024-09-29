776af472e48b5de5394d13ec9d4fd90bd2a56eed
Mohommed Rimaz <rimazmohommed523@gmail.com>
2024-09-29 22:32:53 +1300

    add infrastructure for new angular web app

--- terraform-infra/terraform/linux_web_app.tf
+++ terraform-infra/terraform/linux_web_app.tf
@@ -2,20 +2,17 @@ module "linux_web_app" {
   source   = "./modules/compute/linux_web_app"
   for_each = var.linux_webapps
 
-  resource_group_name   = module.resource_group["app"].name
-  location              = module.resource_group["app"].location
-  environment           = var.environment
-  project               = var.project
-  service_plan_id       = module.service_plan["asp"].id
-  name                  = each.key
-  stack                 = each.value.stack
-  stack_version         = each.value.stack_version
-  app_ssl_cert_name     = module.get_ssl_certificate["rimaz-ssl"].name
-  ssl_cert_kv_secret_id = module.get_ssl_certificate["rimaz-ssl"].secret_id
+  resource_group_name = module.resource_group["app"].name
+  location            = module.resource_group["app"].location
+  environment         = var.environment
+  project             = var.project
+  service_plan_id     = module.service_plan["asp"].id
+  name                = each.key
+  stack               = each.value.stack
+  stack_version       = each.value.stack_version
 
   depends_on = [
     module.resource_group["app"].id,
-    module.service_plan["asp"].id,
-    module.get_ssl_certificate["rimaz-ssl"].secret_id
+    module.service_plan["asp"].id
   ]
 }
--- terraform-infra/terraform/web_app_ssl_custom_domain.tf
+++ terraform-infra/terraform/web_app_ssl_custom_domain.tf
@@ -6,10 +6,11 @@ module "web_app_ssl_custom_domain" {
   web_app_name       = module.linux_web_app[each.value.web_app_key].name
   app_resource_group = module.resource_group["app"].name
   ssl_state          = each.value.ssl_state
-  certificate_id     = module.linux_web_app[each.value.web_app_key].certificate_id
+  certificate_id     = module.app_service_certificate.certificate_id
 
   depends_on = [
     module.linux_web_app["api"].id,
-    module.linux_web_app["react"].id
+    module.linux_web_app["react"].id,
+    module.app_service_certificate.certificate_id
   ]
 }
