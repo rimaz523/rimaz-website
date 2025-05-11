fd553c75845e423326bb6d7760e16c8e6b7c1478
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-05-12 04:40:40 +1200

    add hero to ng app home page (#177)

---
+++ website-angular-spa/src/app/components/hero/hero.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'rmz-hero',
+  imports: [],
+  templateUrl: './hero.component.html',
+  styleUrl: './hero.component.css'
+})
+export class HeroComponent {
+
+}
