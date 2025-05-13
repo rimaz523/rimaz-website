4abe3feded7223b723e8fe117f76d6fd2fe1421b
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-05-14 04:48:32 +1200

    ng add header component

--- website-angular-spa/src/app/app.component.ts
+++ website-angular-spa/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { HomeComponent } from './pages/home/home.component'
+import { HeaderComponent } from './components/header/header.component'
 
 @Component({
   selector: 'rmz-root',
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
