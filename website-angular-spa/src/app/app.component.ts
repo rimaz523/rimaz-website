a9ca91e6c20d62a9c36cdb94753673f8e123c45c
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-05-14 03:53:44 +1200

    format code

--- website-angular-spa/src/app/app.component.ts
+++ website-angular-spa/src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from "./pages/home/home.component";
+import { Component } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { HomeComponent } from './pages/home/home.component'
 
 @Component({
-  selector: 'app-root',
+  selector: 'rmz-root',
   imports: [RouterOutlet, HomeComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'website-angular-spa';
+  title = 'website-angular-spa'
 }
--- website-angular-spa/src/app/components/hero/hero.component.spec.ts
+++ website-angular-spa/src/app/components/hero/hero.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { HeroComponent } from './hero.component';
+import { HeroComponent } from './hero.component'
 
 describe('HeroComponent', () => {
-  let component: HeroComponent;
-  let fixture: ComponentFixture<HeroComponent>;
+  let component: HeroComponent
+  let fixture: ComponentFixture<HeroComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HeroComponent]
-    })
-    .compileComponents();
+      imports: [HeroComponent],
+    }).compileComponents()
 
-    fixture = TestBed.createComponent(HeroComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(HeroComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
