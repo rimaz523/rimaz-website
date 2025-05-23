8c1985e8a73536e0a0885ae264930c401a4460e6
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-05-24 07:11:36 +1200

    angular add toggle between dark and light mode for app (#186)

--- website-angular-spa/src/app/app.component.ts
+++ website-angular-spa/src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
 import { HomeComponent } from './pages/home/home.component'
-import { HeaderComponent } from './components/header/header.component'
+import { HeaderComponent } from './core/components/header/header.component'
 
 @Component({
   selector: 'rmz-root',
