a910dc05aa8924b2472df1b5edd836c34d48e953
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-05-28 04:15:33 +1200

    ng create footer component (#189)
    * ng create footer component
    
    * prettier updates

--- website-angular-spa/src/app/app.component.ts
+++ website-angular-spa/src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router'
 import { HomeComponent } from './pages/home/home.component'
 import { HeaderComponent } from './core/components/header/header.component'
 import { ThemeService } from './core/services/theme.services'
+import { FooterComponent } from './core/components/footer/footer.component'
 
 @Component({
   selector: 'rmz-root',
-  imports: [RouterOutlet, HomeComponent, HeaderComponent],
+  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
