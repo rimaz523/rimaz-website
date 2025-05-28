8080ad8841b18e86ce86bccad6a2d2ec1f54a289
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-05-28 20:08:32 +1200

    ng add footer stories

---
+++ website-angular-spa/src/app/core/components/footer/footer.stories.ts
@@ -0,0 +1,20 @@
+import { componentWrapperDecorator, Meta, StoryObj } from '@storybook/angular'
+import { FooterComponent } from './footer.component'
+
+const meta: Meta<FooterComponent> = {
+  title: 'Core/Footer',
+  component: FooterComponent,
+  tags: ['autodocs'],
+  args: {},
+  decorators: [
+    componentWrapperDecorator(
+      story =>
+        `<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />${story}`,
+    ),
+  ],
+}
+
+export default meta
+
+type Story = StoryObj<FooterComponent>
+export const Default: Story = {}
