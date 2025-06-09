18e016ed2dc6b1d29fa16e2c386059c4269780d0
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-06-09 20:02:16 +1200

    ng setup routes + page not found

--- website-angular-spa/src/app/app.component.ts
+++ website-angular-spa/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
-import { HomeComponent } from './pages/home/home.component'
 import { HeaderComponent } from './core/components/header/header.component'
 import { ThemeService } from './core/services/theme.services'
 import { FooterComponent } from './core/components/footer/footer.component'
 
 @Component({
   selector: 'rmz-root',
-  imports: [RouterOutlet, HomeComponent, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
--- website-angular-spa/src/app/app.routes.ts
+++ website-angular-spa/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router'
+import { HomeComponent } from './pages/home/home.component'
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component'
 
-export const routes: Routes = []
+export enum ROUTER_TOKENS {
+  HOME = '',
+  WILDCARD = '**',
+}
+
+export const routes: Routes = [
+  {
+    path: ROUTER_TOKENS.HOME,
+    component: HomeComponent,
+  },
+  {
+    path: ROUTER_TOKENS.WILDCARD,
+    component: PageNotFoundComponent,
+  },
+]
