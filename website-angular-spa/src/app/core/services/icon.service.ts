73291c042b292240943c2268393483be67fc4f8a
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-06-18 01:08:06 +1200

    ng add constants to icon names and urls

--- website-angular-spa/src/app/core/services/icon.service.ts
+++ website-angular-spa/src/app/core/services/icon.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core'
 import { MatIconRegistry } from '@angular/material/icon'
 import { DomSanitizer } from '@angular/platform-browser'
+import { IconNames, IconResourceUrls } from '@shared/constants/app.constants'
 
 @Injectable({
   providedIn: 'root',
@@ -13,24 +14,24 @@ export class IconService {
 
   init() {
     this.matIconRegistry.addSvgIcon(
-      'medium',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('custom-icons/medium.svg'),
+      IconNames.Medium,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(IconResourceUrls.Medium),
     )
     this.matIconRegistry.addSvgIcon(
-      'stack-overflow',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('custom-icons/stack-overflow.svg'),
+      IconNames.StackOverflow,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(IconResourceUrls.StackOverflow),
     )
     this.matIconRegistry.addSvgIcon(
-      'twitter-x',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('custom-icons/twitter-x.svg'),
+      IconNames.TwitterX,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(IconResourceUrls.TwitterX),
     )
     this.matIconRegistry.addSvgIcon(
-      'linkedin',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('custom-icons/linkedin.svg'),
+      IconNames.Linkedin,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(IconResourceUrls.Linkedin),
     )
     this.matIconRegistry.addSvgIcon(
-      'github',
-      this.domSanitizer.bypassSecurityTrustResourceUrl('custom-icons/github.svg'),
+      IconNames.Github,
+      this.domSanitizer.bypassSecurityTrustResourceUrl(IconResourceUrls.Github),
     )
   }
 }
--- website-angular-spa/src/app/shared/constants/app.constants.ts
+++ website-angular-spa/src/app/shared/constants/app.constants.ts
@@ -1,2 +1,17 @@
 export const APP_THEME = 'app-theme'
 export const DARK_MODE = 'dark-mode'
+
+export const enum IconNames {
+  Github = 'Github',
+  Medium = 'Medium',
+  Linkedin = 'Linkedin',
+  StackOverflow = 'Stackoverflow',
+  TwitterX = 'Twitter',
+}
+export const enum IconResourceUrls {
+  Github = 'custom-icons/github.svg',
+  Medium = 'custom-icons/medium.svg',
+  Linkedin = 'custom-icons/linkedin.svg',
+  StackOverflow = 'custom-icons/stack-overflow.svg',
+  TwitterX = 'custom-icons/twitter-x.svg',
+}
