6ec265f0c166c8a4900cbc1620eef57095ff1bda
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-06-24 00:47:46 +1200

    ng use delcarative approach for social handles https observable (#203)

--- website-angular-spa/src/app/components/social-handles-band/social-handles-band.component.ts
+++ website-angular-spa/src/app/components/social-handles-band/social-handles-band.component.ts
@@ -1,25 +1,18 @@
-import { Component } from '@angular/core'
+import { Component, inject } from '@angular/core'
 import { MatDividerModule } from '@angular/material/divider'
 import { MatIconModule } from '@angular/material/icon'
 import { SocialHandleComponent } from '@components/social-handle/social-handle.component'
-import { ISocialHandle } from '@models/social-handle.model'
-import { NgFor } from '@angular/common'
+import { AsyncPipe } from '@angular/common'
 import { SocialHandlesService } from '@components/social-handles-band/social-handles.service'
+import { shareReplay } from 'rxjs'
 
 @Component({
   selector: 'rmz-social-handles-band',
-  imports: [MatDividerModule, MatIconModule, SocialHandleComponent, NgFor],
+  imports: [MatDividerModule, MatIconModule, SocialHandleComponent, AsyncPipe],
   templateUrl: './social-handles-band.component.html',
   styleUrl: './social-handles-band.component.scss',
 })
 export class SocialHandlesBandComponent {
-  socialHandles!: ISocialHandle[]
-
-  constructor(private socialHandlesService: SocialHandlesService) {
-    socialHandlesService.getSocialHandles().subscribe(handles => (this.socialHandles = handles))
-  }
-
-  getOrderedSocialHandles(): ISocialHandle[] {
-    return this.socialHandles.sort((a, b) => a.displayOrder - b.displayOrder)
-  }
+  private socialHandlesService = inject(SocialHandlesService)
+  readonly socialHandles$ = this.socialHandlesService.socialHandles$.pipe(shareReplay(1))
 }
--- website-angular-spa/src/app/components/social-handles-band/social-handles.service.ts
+++ website-angular-spa/src/app/components/social-handles-band/social-handles.service.ts
@@ -1,22 +1,18 @@
 import { HttpClient } from '@angular/common/http'
-import { Injectable } from '@angular/core'
+import { inject, Injectable } from '@angular/core'
 import { ISocialHandle } from '@models/social-handle.model'
-import { Observable } from 'rxjs'
 
 @Injectable({
   providedIn: 'root',
 })
 export class SocialHandlesService {
-  constructor(private http: HttpClient) {}
-
-  getSocialHandles(): Observable<ISocialHandle[]> {
-    return this.http.get<ISocialHandle[]>(
-      'https://rimaz-dev-backend-apim.azure-api.net/v1/api/socialhandles',
-      {
-        headers: {
-          'ocp-apim-subscription-key': 'f7c3ca94b5b94e6ab7bec64a983c3fb9',
-        },
+  private http = inject(HttpClient)
+  readonly socialHandles$ = this.http.get<ISocialHandle[]>(
+    'https://rimaz-dev-backend-apim.azure-api.net/v1/api/socialhandles',
+    {
+      headers: {
+        'ocp-apim-subscription-key': 'f7c3ca94b5b94e6ab7bec64a983c3fb9',
       },
-    )
-  }
+    },
+  )
 }
