dc84e45b888d8374ee3b29857f6761816ceed6c5
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-06-28 02:55:48 +1200

    ng use signals in social handles band component (#204)
    * ng use signals in social handles band component
    
    * use @if instead of ngIf

--- website-angular-spa/src/app/components/social-handles-band/social-handles.service.ts
+++ website-angular-spa/src/app/components/social-handles-band/social-handles.service.ts
@@ -1,18 +1,21 @@
 import { HttpClient } from '@angular/common/http'
 import { inject, Injectable } from '@angular/core'
 import { ISocialHandle } from '@models/social-handle.model'
+import { toSignal } from '@angular/core/rxjs-interop'
+import { shareReplay } from 'rxjs'
 
 @Injectable({
   providedIn: 'root',
 })
 export class SocialHandlesService {
   private http = inject(HttpClient)
-  readonly socialHandles$ = this.http.get<ISocialHandle[]>(
-    'https://rimaz-dev-backend-apim.azure-api.net/v1/api/socialhandles',
-    {
+  private readonly socialHandles$ = this.http
+    .get<ISocialHandle[]>('https://rimaz-dev-backend-apim.azure-api.net/v1/api/socialhandles', {
       headers: {
         'ocp-apim-subscription-key': 'f7c3ca94b5b94e6ab7bec64a983c3fb9',
       },
-    },
-  )
+    })
+    .pipe(shareReplay(1))
+
+  readonly socialHandles = toSignal(this.socialHandles$, { initialValue: [] as ISocialHandle[] })
 }
