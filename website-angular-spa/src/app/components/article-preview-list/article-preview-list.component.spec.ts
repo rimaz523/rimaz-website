d56e6a1131750d8551758d8b54c9827e9b5ead2f
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-07-02 08:59:45 +1200

    formatting

--- website-angular-spa/src/app/components/article-preview-list/article-preview-list.component.spec.ts
+++ website-angular-spa/src/app/components/article-preview-list/article-preview-list.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { ArticlePreviewListComponent } from './article-preview-list.component';
+import { ArticlePreviewListComponent } from './article-preview-list.component'
 
 describe('ArticlePreviewListComponent', () => {
-  let component: ArticlePreviewListComponent;
-  let fixture: ComponentFixture<ArticlePreviewListComponent>;
+  let component: ArticlePreviewListComponent
+  let fixture: ComponentFixture<ArticlePreviewListComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ArticlePreviewListComponent]
-    })
-    .compileComponents();
+      imports: [ArticlePreviewListComponent],
+    }).compileComponents()
 
-    fixture = TestBed.createComponent(ArticlePreviewListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(ArticlePreviewListComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
