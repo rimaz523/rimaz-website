e4e1b0aa2c5dc5ed52aa68a29a6987cacd4e60b0
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-07-03 01:53:57 +1200

    ng make article preview card clickable + css updates (#209)

--- website-angular-spa/src/app/components/article-preview/article-preview.component.ts
+++ website-angular-spa/src/app/components/article-preview/article-preview.component.ts
@@ -1,13 +1,18 @@
 import { Component, Input } from '@angular/core'
+import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
 import { IArticle } from '@models/article.model'
 
 @Component({
   selector: 'rmz-article-preview',
-  imports: [MatCardModule],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './article-preview.component.html',
   styleUrl: './article-preview.component.scss',
 })
 export class ArticlePreviewComponent {
   @Input() article!: IArticle
+
+  openLinkInNewTab(url: string) {
+    window.open(url, '_blank')
+  }
 }
