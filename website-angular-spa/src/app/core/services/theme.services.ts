66d8e02693e3b3bbc2671378de4a1cfa651f76ee
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-07-10 22:36:35 +1200

    ng remove hardcoding of hero image url

--- website-angular-spa/src/app/core/services/theme.services.ts
+++ website-angular-spa/src/app/core/services/theme.services.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@angular/core'
+import { Injectable, signal } from '@angular/core'
 import { APP_THEME, DARK_MODE } from '@shared/constants/app.constants'
 
 @Injectable({ providedIn: 'root' })
 export class ThemeService {
-  isDark = false
+  isDark = signal(false)
 
   constructor() {
     this.loadTheme()
@@ -13,27 +13,27 @@ export class ThemeService {
     const theme = localStorage.getItem(APP_THEME)
     if (theme === DARK_MODE) {
       document.documentElement.classList.add(DARK_MODE)
-      this.isDark = true
+      this.isDark.set(true)
     } else {
       document.documentElement.classList.remove(DARK_MODE)
     }
   }
 
   setDarkMode(): boolean {
-    this.isDark = true
+    this.isDark.set(true)
     document.documentElement.classList.add(DARK_MODE)
     localStorage.setItem(APP_THEME, DARK_MODE)
-    return this.isDark
+    return this.isDark()
   }
 
   setLightMode(): boolean {
-    this.isDark = false
+    this.isDark.set(false)
     document.documentElement.classList.remove(DARK_MODE)
     localStorage.removeItem(APP_THEME)
-    return this.isDark
+    return this.isDark()
   }
 
   getIsDark(): boolean {
-    return this.isDark
+    return this.isDark()
   }
 }
