b158d5717b3d767b2a3947ed896ae37ea6aca627
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-07-18 19:09:32 +1200

    upgrade storybook to v9

--- website-angular-spa/.storybook/main.ts
+++ website-angular-spa/.storybook/main.ts
@@ -2,11 +2,7 @@ import type { StorybookConfig } from '@storybook/angular'
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
-  addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-onboarding',
-    '@storybook/addon-interactions',
-  ],
+  addons: ['@storybook/addon-onboarding', '@storybook/addon-docs'],
   framework: {
     name: '@storybook/angular',
     options: {},
