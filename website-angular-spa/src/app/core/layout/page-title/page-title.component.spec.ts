6809e2612925288c41c64976257f6e6b5344646d
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-08-13 11:05:14 +1200

    linting updates

--- website-angular-spa/src/app/core/layout/page-title/page-title.component.spec.ts
+++ website-angular-spa/src/app/core/layout/page-title/page-title.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing'
 
-import { PageTitleComponent } from './page-title.component';
+import { PageTitleComponent } from './page-title.component'
 
 describe('PageTitleComponent', () => {
-  let component: PageTitleComponent;
-  let fixture: ComponentFixture<PageTitleComponent>;
+  let component: PageTitleComponent
+  let fixture: ComponentFixture<PageTitleComponent>
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PageTitleComponent]
-    })
-    .compileComponents();
+      imports: [PageTitleComponent],
+    }).compileComponents()
 
-    fixture = TestBed.createComponent(PageTitleComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    fixture = TestBed.createComponent(PageTitleComponent)
+    component = fixture.componentInstance
+    fixture.detectChanges()
+  })
 
   it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+    expect(component).toBeTruthy()
+  })
+})
