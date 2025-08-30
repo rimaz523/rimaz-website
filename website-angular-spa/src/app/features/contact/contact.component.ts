1342526da3fb955738c2c2f7a15279b5594d40fd
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-08-30 15:55:52 +1200

    ng validate contact form (#234)

--- website-angular-spa/src/app/features/contact/contact.component.ts
+++ website-angular-spa/src/app/features/contact/contact.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core'
-import { FormControl, ReactiveFormsModule } from '@angular/forms'
+import { Component, inject } from '@angular/core'
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms'
 import { MatButtonModule } from '@angular/material/button'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatInputModule } from '@angular/material/input'
 
 import { PageTitleComponent } from '@core/layout/page-title/page-title.component'
+import { ContactService } from './contact.service'
 
 @Component({
   selector: 'rmz-contact',
@@ -19,7 +20,18 @@ import { PageTitleComponent } from '@core/layout/page-title/page-title.component
   styleUrl: './contact.component.scss',
 })
 export class ContactComponent {
-  name = new FormControl()
-  email = new FormControl()
-  message = new FormControl()
+  private contactService = inject(ContactService)
+  private fb = inject(FormBuilder)
+
+  contactForm = this.fb.nonNullable.group({
+    name: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    message: ['', Validators.required],
+  })
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      this.contactService.sendMessage(this.contactForm.getRawValue())
+    }
+  }
 }
