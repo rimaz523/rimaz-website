56aa05d0ede1fdc42cfa8e38ba32673335f07786
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-08-31 17:07:00 +1200

    ng separate out the loader as its own component

---
+++ website-angular-spa/src/app/core/layout/loader/loader.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core'
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner'
+
+@Component({
+  selector: 'rmz-loader',
+  imports: [MatProgressSpinnerModule],
+  templateUrl: './loader.component.html',
+  styleUrl: './loader.component.scss',
+})
+export class LoaderComponent {}
