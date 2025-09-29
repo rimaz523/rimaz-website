7712b91e1a02413e2b2a4e4930e7be7910e92d3d
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-09-29 21:27:05 +1300

    fix formatting

--- integrations-dotnet-api/src/Application/Articles/Queries/GetArticle/ArticleDto.cs
+++ integrations-dotnet-api/src/Application/Articles/Queries/GetArticle/ArticleDto.cs
@@ -9,6 +9,6 @@ public class ArticleDto : IMapFrom<Article>
     public string Title { get; set; } = string.Empty;
     public string Slug { get; set; } = string.Empty;
     public string Image { get; set; } = string.Empty;
-    public string Content {  get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
     public ICollection<Section> Sections { get; set; } = new List<Section>();
 }
