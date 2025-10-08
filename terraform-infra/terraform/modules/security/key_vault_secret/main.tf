2abf75500684de2d0554e00e49044d8d6399e0a4
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-10-08 21:15:09 +1300

    ignore kv values once set

--- terraform-infra/terraform/modules/security/key_vault_secret/main.tf
+++ terraform-infra/terraform/modules/security/key_vault_secret/main.tf
@@ -5,4 +5,8 @@ resource "azurerm_key_vault_secret" "kv_secret" {
   name         = var.secret_name
   value        = var.secret_value
   key_vault_id = var.key_vault_id
+
+  lifecycle {
+    ignore_changes = [value]
+  }
 }
