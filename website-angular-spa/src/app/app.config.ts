bb35ae3c69aeb9380d9cb7b2873b9e245324642b
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-11-01 14:15:04 +1300

    configure msal for external id b2c (#251)

--- website-angular-spa/src/app/app.config.ts
+++ website-angular-spa/src/app/app.config.ts
@@ -3,19 +3,115 @@ import {
   inject,
   provideAppInitializer,
   provideZoneChangeDetection,
+  importProvidersFrom,
+  APP_INITIALIZER,
 } from '@angular/core'
 import { provideRouter } from '@angular/router'
-
+import { provideHttpClient, withInterceptors, HTTP_INTERCEPTORS } from '@angular/common/http'
+import {
+  IPublicClientApplication,
+  PublicClientApplication,
+  InteractionType,
+  BrowserCacheLocation,
+  LogLevel,
+} from '@azure/msal-browser'
+import {
+  MsalInterceptor,
+  MSAL_INSTANCE,
+  MsalInterceptorConfiguration,
+  MsalGuardConfiguration,
+  MSAL_GUARD_CONFIG,
+  MSAL_INTERCEPTOR_CONFIG,
+  MsalService,
+  MsalGuard,
+  MsalBroadcastService,
+} from '@azure/msal-angular'
+import { BrowserModule } from '@angular/platform-browser'
 import { routes } from './app.routes'
 import { IconService } from './core/services/icon.service'
-import { provideHttpClient, withInterceptors } from '@angular/common/http'
 import { apimSubscriptionHeaderInterceptor } from './core/interceptors/apim-subscription-header.interceptor'
+import { environment } from '../environments/environment'
+
+export function loggerCallback(logLevel: LogLevel, message: string) {
+  console.log(message)
+}
+
+export function MSALInstanceFactory(): IPublicClientApplication {
+  return new PublicClientApplication({
+    auth: {
+      clientId: environment.ms_external_id_client_id,
+      authority: `https://${environment.ms_external_id_subdomain}.ciamlogin.com/`,
+      redirectUri: `${environment.ms_external_id_spa_url}auth`,
+      postLogoutRedirectUri: `${environment.ms_external_id_spa_url}`,
+    },
+    cache: {
+      cacheLocation: BrowserCacheLocation.LocalStorage,
+    },
+    system: {
+      allowPlatformBroker: false, // Disables WAM Broker
+      loggerOptions: {
+        loggerCallback,
+        logLevel: LogLevel.Info,
+        piiLoggingEnabled: false,
+      },
+    },
+  })
+}
+
+export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
+  const protectedResourceMap = new Map<string, string[]>()
+  protectedResourceMap.set('https://graph.microsoft.com/v1.0/me', ['user.read'])
+
+  return {
+    interactionType: InteractionType.Redirect,
+    protectedResourceMap,
+  }
+}
+
+export function MSALGuardConfigFactory(): MsalGuardConfiguration {
+  return {
+    interactionType: InteractionType.Redirect,
+    authRequest: {
+      scopes: ['user.read'],
+    },
+    loginFailedRoute: '/login-failed',
+  }
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideHttpClient(withInterceptors([apimSubscriptionHeaderInterceptor])),
+    importProvidersFrom(BrowserModule),
     provideAppInitializer(() => inject(IconService).init()),
+    provideHttpClient(withInterceptors([apimSubscriptionHeaderInterceptor])),
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: MsalInterceptor,
+      multi: true,
+    },
+    {
+      provide: MSAL_INSTANCE,
+      useFactory: MSALInstanceFactory,
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: (msalService: MsalService) => {
+        return () => msalService.instance.initialize() // Ensure initialize is called
+      },
+      deps: [MsalService],
+      multi: true,
+    },
+    {
+      provide: MSAL_GUARD_CONFIG,
+      useFactory: MSALGuardConfigFactory,
+    },
+    {
+      provide: MSAL_INTERCEPTOR_CONFIG,
+      useFactory: MSALInterceptorConfigFactory,
+    },
+    MsalService,
+    MsalGuard,
+    MsalBroadcastService,
   ],
 }
--- website-angular-spa/src/app/core/layout/side-menu/side-menu.component.ts
+++ website-angular-spa/src/app/core/layout/side-menu/side-menu.component.ts
@@ -1,4 +1,4 @@
-import { Component, effect, inject, viewChild } from '@angular/core'
+import { Component, effect, Inject, inject, OnDestroy, OnInit, viewChild } from '@angular/core'
 import { RouterLink, RouterOutlet, RouterLinkActive } from '@angular/router'
 import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav'
 import { SideMenuService } from '@core/services/side-menu.service'
@@ -6,11 +6,19 @@ import { MatDividerModule } from '@angular/material/divider'
 import { MatListModule } from '@angular/material/list'
 import { MatButtonModule } from '@angular/material/button'
 import { MatIcon } from '@angular/material/icon'
-import { ThemeService } from '@core/services/theme.services'
+import {
+  MsalGuardConfiguration,
+  MSAL_GUARD_CONFIG,
+  MsalService,
+  MsalBroadcastService,
+} from '@azure/msal-angular'
+import { RedirectRequest, EventType, EventMessage, InteractionStatus } from '@azure/msal-browser'
 
 import { RouterTokens } from 'app/app.routes'
 import { environment } from 'environments/environment'
 import { BrowserUtilsService } from '@core/services/browser-utils.service'
+import { ThemeService } from '@core/services/theme.services'
+import { filter, Subject, takeUntil } from 'rxjs'
 
 @Component({
   selector: 'rmz-side-menu',
@@ -27,18 +35,24 @@ import { BrowserUtilsService } from '@core/services/browser-utils.service'
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.scss',
 })
-export class SideMenuComponent {
+export class SideMenuComponent implements OnInit, OnDestroy {
   private themeService = inject(ThemeService)
   private sideMenuService = inject(SideMenuService)
+  private authService = inject(MsalService)
+  private msalBroadcastService = inject(MsalBroadcastService)
+  private readonly _destroying$ = new Subject<void>()
 
   readonly browserUtilsService = inject(BrowserUtilsService)
   readonly sidenav = viewChild<MatSidenav>('sidenav')
   readonly routerTokens = RouterTokens
 
+  loginDisplay = false
+  userFirstName = ''
   githubReportIssueUrl = environment.githubReportIssueUrl
   isDark = false
 
-  constructor() {
+  // eslint-disable-next-line @angular-eslint/prefer-inject
+  constructor(@Inject(MSAL_GUARD_CONFIG) private msalGuardConfig: MsalGuardConfiguration) {
     this.isDark = this.themeService.getIsDark()
     effect(() => {
       if (this.sideMenuService.isOpen()) {
@@ -47,6 +61,77 @@ export class SideMenuComponent {
         this.closeSidenav()
       }
     })
+  }
+
+  async ngOnInit(): Promise<void> {
+    this.authService.handleRedirectObservable().subscribe()
+    this.setLoginDisplay()
+    this.authService.instance.enableAccountStorageEvents() // Optional - This will enable ACCOUNT_ADDED and ACCOUNT_REMOVED events emitted when a user logs in or out of another tab or window
+    this.msalBroadcastService.msalSubject$
+      .pipe(
+        filter(
+          (msg: EventMessage) =>
+            msg.eventType === EventType.ACCOUNT_ADDED ||
+            msg.eventType === EventType.ACCOUNT_REMOVED,
+        ),
+      )
+      .subscribe(() => {
+        if (this.authService.instance.getAllAccounts().length === 0) {
+          window.location.pathname = '/'
+        } else {
+          this.setLoginDisplay()
+        }
+      })
+
+    this.msalBroadcastService.inProgress$
+      .pipe(
+        filter((status: InteractionStatus) => status === InteractionStatus.None),
+        takeUntil(this._destroying$),
+      )
+      .subscribe(() => {
+        this.setLoginDisplay()
+        this.checkAndSetActiveAccount()
+      })
+  }
+
+  signIn(): void {
+    this.closeSidenav()
+    if (this.msalGuardConfig.authRequest) {
+      this.authService.loginRedirect({
+        ...this.msalGuardConfig.authRequest,
+      } as RedirectRequest)
+    } else {
+      this.authService.loginRedirect()
+    }
+  }
+
+  signOut() {
+    const activeAccount =
+      this.authService.instance.getActiveAccount() || this.authService.instance.getAllAccounts()[0]
+    this.authService.logoutRedirect({ account: activeAccount })
+  }
+
+  checkAndSetActiveAccount() {
+    /**
+     * If no active account set but there are accounts signed in, sets first account to active account
+     * To use active account set here, subscribe to inProgress$ first in your component
+     * Note: Basic usage demonstrated. Your app may require more complicated account selection logic
+     */
+    const activeAccount = this.authService.instance.getActiveAccount()
+
+    if (!activeAccount && this.authService.instance.getAllAccounts().length > 0) {
+      console.log('No active account! Setting the first account as active account')
+      const accounts = this.authService.instance.getAllAccounts()
+      // add your code for handling multiple accounts here
+      this.authService.instance.setActiveAccount(accounts[0])
+    }
+  }
+
+  setLoginDisplay() {
+    this.loginDisplay = this.authService.instance.getAllAccounts().length > 0
+    console.log('Login Display set to ' + this.loginDisplay)
+    console.log(this.authService.instance.getAllAccounts()[0])
+    this.userFirstName = this.authService.instance.getAllAccounts()[0]?.name || ''
   }
 
   setDarkMode(): void {
@@ -66,5 +151,10 @@ export class SideMenuComponent {
   closeSidenav() {
     this.sideMenuService.closeSidenav()
     this.sidenav()?.close()
+  }
+
+  ngOnDestroy(): void {
+    this._destroying$.next(undefined)
+    this._destroying$.complete()
   }
 }
