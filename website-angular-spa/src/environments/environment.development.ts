9e9f2de7365e833557203bbebdcc8c48c7949581
Mohommed Rimaz <rimazmohommed523@gmail.com>
2025-11-01 13:54:06 +1300

    configure msal for external id b2c

--- website-angular-spa/src/environments/environment.development.ts
+++ website-angular-spa/src/environments/environment.development.ts
@@ -4,4 +4,7 @@ export const environment = {
   cdnUrl: 'http://127.0.0.1:10000/devstoreaccount1',
   githubReportIssueUrl: 'https://github.com/rimaz523/rimaz-website/issues/new',
   downloadResumeUrl: 'https://raw.githubusercontent.com/rimaz523/Resume/master/RimazMohommed.pdf',
+  ms_external_id_client_id: '52193716-33b5-4ef3-b6fb-9be16941a495',
+  ms_external_id_subdomain: 'rimazdev',
+  ms_external_id_spa_url: 'http://localhost:4200/',
 }
