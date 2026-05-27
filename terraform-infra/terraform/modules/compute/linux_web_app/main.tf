be46e6a1b7cac95efb1c0e837da0202b9d27ee52
Mohommed Rimaz <rimazmohommed523@gmail.com>
2026-05-27 20:23:28 +1200

    640 - disable scm access to all web apps

--- terraform-infra/terraform/modules/compute/linux_web_app/main.tf
+++ terraform-infra/terraform/modules/compute/linux_web_app/main.tf
@@ -9,9 +9,10 @@ resource "azurerm_linux_web_app" "webapp_linux" {
   https_only          = true
 
   site_config {
-    always_on                     = var.always_on
-    app_command_line              = var.stack == "node" ? "pm2 serve /home/site/wwwroot --spa --no-daemon" : null
-    ip_restriction_default_action = "Deny"
+    always_on                         = var.always_on
+    app_command_line                  = var.stack == "node" ? "pm2 serve /home/site/wwwroot --spa --no-daemon" : null
+    ip_restriction_default_action     = "Deny"
+    scm_ip_restriction_default_action = "Deny"
 
     application_stack {
       dotnet_version = var.stack == "dotnet" ? var.stack_version : null
